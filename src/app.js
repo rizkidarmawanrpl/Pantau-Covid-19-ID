aebb1175b212cbbc7683b560b487a4855710c072
Erdeprof <rizkidarmawanrpl@gmail.com>
2020-10-02 13:08:46 +0700

    Commit add desain statistik covid19

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import "regenerator-runtime";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./style/style.css";
 import "lightslider/dist/css/lightslider.css";
+import "bootstrap-icons/bootstrap-icons.svg";
 
 import "lightslider/dist/js/lightslider.js"
 import main from "./script/controllers/main.js";
--- src/script/controllers/main.js
+++ src/script/controllers/main.js
@@ -5,6 +5,8 @@ const main = () => {
         autoWidth: true,
         loop: true,
         pauseOnHover: true,
+        slideMargin: 35,
+        pause: 4000,
         onSliderLoad: function() {
             $('#autoWidth').removeClass('cS-hidden');
         }
